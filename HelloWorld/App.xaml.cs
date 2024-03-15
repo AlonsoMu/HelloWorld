dc765634af6a8a4687da353b67cbcb9c269a3a24
AlonsoMu <alonsomunoz263@gmail.com>
2024-03-15 16:53:20 -0500

    Agregar archivos de proyecto.

---
+++ HelloWorld/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HelloWorld.Vistas;
+
+namespace HelloWorld
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MenuPrincipal();
+            //Debemos activar la navegacion
+            MainPage = new NavigationPage(new MenuPrincipal());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HelloWorld/Vistas/MenuPrincipal.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPrincipal : ContentPage
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarApp_Clicked(object sender, EventArgs e)
+        {
+            //Abrir una nueva pantalla (Activity = XAML + CS)
+            Navigation.PushAsync(new Convertir());
+        }
+    }
+}
